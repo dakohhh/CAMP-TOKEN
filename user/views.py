78280d7e14b5250ccc3aa98c92def8ea0eafa8dd
Dakoh Wisdom <wiizzydreadmill@gmail.com>
2023-05-17 21:56:43 -1200

    modified code

---
+++ user/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
+
+# Create your views here.
+
+def home(request:HttpRequest):
+    
+    return HttpResponse("Esmf")
+
+
+
+
+def signup_student(requuest:HttpRequest):
+
+    
+
+
+    pass
